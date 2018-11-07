b455fc8e3b339b3f2baa26251105d6f6c0c49c27
Sahir Nambiar <sahirnambiar@gmail.com>
2018-11-07 17:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var emp = [];
+  for(var i=0;i<musicians.length;i++) {
+    emp.push(`${musicians[i]} plays ${instruments[i]}`);
+  }
+  return emp;
+}
+
+function johnLennonFacts(facts) {
+  var factso = [];
+  var i = 0;
+  while (i<facts.length) {
+    factso.push(facts[i] + '!!!');
+    i++;
+  }
+  return factso;
+}
+
+function iLoveTheBeatles(n) {
+  var apo = [];
+  var i = 0;
+  do {
+    apo.push('I love the Beatles!');
+    i++;
+  }
+  while(i<15-n);
+  return apo;
+}
